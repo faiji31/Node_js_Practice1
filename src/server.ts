5cf98b2f959ca273cb9185f05221f3668caf50dd
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-04 21:47:27 +0600

    3rd commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server : Server = createServer((req : IncomingMessage,res)=>{
-    console.log(req)
+    // console.log(req.url)
+    // console.log(req.method)
+
+    const url = req.url
+    const method = req.method
+
+    if(url === "/" && method === "GET"){
+       res.writeHead(200,{"content-type":"text/plain"})
+       res.end("This is The Route Server!")
+    }else{
+        res.writeHead(404,{"content-type":"text/plain"})
+        res.end("Route not found")
+    }
 })
 
 server.listen(5000,()=>{
